d04e96ca84e09c62f3c48cc846c9bb1da8cffbe3
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-07-31 16:25:34 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB54-JPIKrtcZn_EHN8yzduIkdpttZEQb8",
+  authDomain: "ap24-82825.firebaseapp.com",
+  databaseURL: "https://ap24-82825-default-rtdb.firebaseio.com",
+  projectId: "ap24-82825",
+  storageBucket: "ap24-82825.appspot.com",
+  messagingSenderId: "553997269558",
+  appId: "1:553997269558:web:01f33458878f20dc267697",
+  measurementId: "G-LNMQ3CTKJE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("loginForm").addEventListener("submit", function(e) {
         e.preventDefault();
